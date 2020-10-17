b14edd045131a18307c666302ed1d7ed9df1447b
brunoharlein <bruno@harlein.com>
2020-10-17 11:54:46 +0200

    begin loops and conditions

--- main.py
+++ main.py
@@ -14,3 +14,21 @@ computer_number = random.randint(1, 20)
 
 # display rules of the game
 print("Bien,  " + player + ", je pense à un nombre entre 1 et 20.")
+
+# for 6 game rounds, execute ...
+for guessestaken in range(6):
+    print("Essaie de le deviner")
+
+    # choice number user
+    guess = input()
+    # "transtypage" we are waiting for an integer
+    guess = int(guess)
+
+    if guess < computer_number:
+        print("Trop petit !")
+
+    if guess > computer_number:
+        print("Trop grand !")
+
+    if guess == computer_number:
+        break
